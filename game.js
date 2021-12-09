7c50df657ba1589afa9eb65f7c33cd50fd6af49f
Kyle Evans <krevan88@protonmail.com>
2021-12-09 16:36:43 -0700

    titan stops attacking when ded

--- game.js
+++ game.js
@@ -2,12 +2,14 @@ let characters = {
     Hero: {
         health: 100,
         attack: 5,
-        img: 'http://placehold.it/634x634'
+        img: 'http://placehold.it/634x634',
+        bar: 'bg-success'
     },
     Titan: {
         health: 100,
         attack: 5,
-        img: 'http://thiscatdoesnotexist.com'
+        img: 'http://thiscatdoesnotexist.com',
+        bar: 'bg-danger'
     }
 }
 
@@ -19,46 +21,39 @@ function drawChars() {
         <div class="col-6 text-center my-3">
             <h3>${key}</h3> 
             <div class="progress">
-                <div id="${key}-health" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow= "${chars.health}" aria-valuemin="0" aria-valuemax="100" style="width: ${chars.health}%"></div> 
+                <div id="${key}-health" class="${chars.bar} progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow= "${chars.health}" aria-valuemin="0" aria-valuemax="100" style="width: ${chars.health}%"></div> 
             </div>
-            <img class="m-4" src="${chars.img}" alt="${key}" style="width: 90%;" onclick="attack('${key}')
-    "></div>`
+            <img class="m-4" src="${chars.img}" alt="${key}" style="width: 90%;" onclick="attack('${key}')">
+            </div>`
     }
     document.getElementById('characters').innerHTML = template
 }
 
 function drawCharHealth(charsKey) {
-    console.log(charsKey)
     document.getElementById(`${charsKey}-health`).style = `width: ${characters[charsKey].health}%`
 }
 
-function decrementHealth(charsKey) {
-    for (const key in characters) {
-        if (characters[key].health > 0) {
-            characters[key].health -= 5
-            drawCharHealth(key)
-        }
-
-    }
-}
-
 function attack(charsKey) {
-    console.log('This works.')
     const titan = characters[charsKey]
-    if (titan.health > 0) {
-        titan.health -= 5
+    if (charsKey == 'Titan') {
+        if (titan.health > 0) {
+            titan.health -= 5
+        }
     }
     drawCharHealth(charsKey)
 }
 
-function healHero() {
-    console.log('MEDPACK')
+function decrementHealth(charsKey) {
+    if (characters.Hero.health > 0 && characters.Titan.health > 0) {
+        characters.Hero.health -= 5
+        drawCharHealth('Hero')
+    }
 }
 
 
+function startInterval(charsKey) {
+    setInterval(decrementHealth, 3000)
+}
 
-
-
-
-
+startInterval()
 drawChars()
